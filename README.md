c45ec82e3f2b604ceb539c3bc43b8c416907b62e
xPaw <me@xpaw.ru>
2013-06-10 17:49:51 +0300

    Add more tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Convert-SteamID
-===============
+SteamID Converter [![Build Status](https://travis-ci.org/xPaw/SteamID.png?branch=master)](https://travis-ci.org/xPaw/SteamID)
+=================
 
 JavaScript implemention of SteamID converter
--- test/SteamID-test.js
+++ test/SteamID-test.js
@@ -2,7 +2,7 @@ var assert = require( 'assert' ),
     vows = require( 'vows' ),
     SteamID = require( '../' );
 
-vows.describe( 'SteamID' ).addBatch(
+vows.describe( 'SteamID - set functions' ).addBatch(
 {
 	'Steam2 to CommunityID':
 	{
@@ -10,16 +10,16 @@ vows.describe( 'SteamID' ).addBatch(
 		'result should be valid': function( result )
 		{
 			assert.isString( result );
-			assert.equal( result, '76561197972494985' );
+			assert.strictEqual( result, '76561197972494985' );
 		}
 	},
 	'CommunityID to Steam2':
 	{
-		topic: new SteamID( ).SetCommunityID( '76561197972494985' ).GetCommunityID( ),
+		topic: new SteamID( ).SetCommunityID( '76561197972494985' ).GetSteam2( ),
 		'result should be valid': function( result )
 		{
 			assert.isString( result );
-			assert.equal( result, 'STEAM_0:1:6114628' );
+			assert.strictEqual( result, 'STEAM_0:1:6114628' );
 		}
 	},
 	'CommunityID to Steam3':
@@ -28,16 +28,50 @@ vows.describe( 'SteamID' ).addBatch(
 		'result should be valid': function( result )
 		{
 			assert.isString( result );
-			assert.equal( result, '[U:1:12229257]' );
+			assert.strictEqual( result, '[U:1:12229257]' );
 		}
 	},
-	'AccountID to Steam2':
+	'AccountID (numeric) to Steam2':
 	{
 		topic: new SteamID( ).SetAccountID( 12229257 ).GetSteam2( ),
 		'result should be valid': function( result )
 		{
 			assert.isString( result );
-			assert.equal( result, 'STEAM_0:1:6114628' );
+			assert.strictEqual( result, 'STEAM_0:1:6114628' );
 		}
-	}
+	},
+	'AccountID (string) to Steam2':
+	{
+		topic: new SteamID( ).SetAccountID( '12229257' ).GetSteam2( ),
+		'result should be valid': function( result )
+		{
+			assert.isString( result );
+			assert.strictEqual( result, 'STEAM_0:1:6114628' );
+		}
+	},
+	'CommunityID to AccountID':
+	{
+		topic: new SteamID( ).SetCommunityID( '76561197972494985' ).GetAccountID( ),
+		'result should be valid': function( result )
+		{
+			assert.isNumber( result );
+			assert.strictEqual( result, 12229257 );
+		}
+	},
+	'Set CommunityID as number':
+	{
+		topic: new SteamID( ).SetCommunityID( 76561197972494985 ),
+		'result should be false': function( result )
+		{
+			assert.isFalse( result );
+		}
+	},
+	'Set invalid Steam2':
+	{
+		topic: new SteamID( ).SetSteam2( 'STEAM_ID_PENDING' ),
+		'result should be false': function( result )
+		{
+			assert.isFalse( result );
+		}
+	},
 } ).export( module );
